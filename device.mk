5b1f608dd7b28aa4beb4697de2f78a1bf4f1cb91
YudhoPatrianto <kydh01123@gmail.com>
2024-01-26 13:37:35 +0700

    fire: Initial DT For PBRP
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

---
+++ device.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/xiaomi/fire
+# A/B
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6768 \
+    bootctrl.mt6768.recovery
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
---
+++ twrp_fire.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from fire device
+$(call inherit-product, device/xiaomi/fire/device.mk)
+
+PRODUCT_DEVICE := fire
+PRODUCT_NAME := twrp_fire
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := fire
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi 12
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
