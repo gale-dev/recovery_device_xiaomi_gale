748a8643cac73cb0c31b8bd28d8a690f5b0e27a1
YudhoPatrianto <kydh01123@gmail.com>
2024-02-01 02:36:22 +0700

    Revert "fire: switch to omni_"
    This reverts commit c7d6639598a40555c6046d55ec1519055e2525d2.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's omni device tree generator
+# Copyright (C) 2024 SebaUbuntu's pb device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_fire.mk
+    $(LOCAL_DIR)/pb_fire.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_fire-eng
+    pb_fire-eng
