3bbfdfc2c9a6da7104f806b45eea812d9721142f
YudhoPatrianto <kydh01123@gmail.com>
2024-02-05 18:07:24 +0700

    fire: BoardConfig:try fix maintainer flags
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -184,7 +184,7 @@ PB_DISABLE_DEFAULT_TREBLE_COMP := true
 PB_DISABLE_DEFAULT_DM_VERITY := true
 
 # Maintainer Flags
-MTAINER := YudhoPatrianto
+MAINTAINER := 'YudhoPatrianto'
 
 # Libresetprop & resetprop
 TW_INCLUDE_LIBRESETPROP := true
