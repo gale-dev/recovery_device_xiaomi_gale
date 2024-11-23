2be21a5d36bcdf943896a551e441b5946b9dd2c5
wulan17 <wulan17@nusantararom.org>
2024-11-23 23:42:50 +0700

    gale: Cleanup
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- device.mk
+++ device.mk
@@ -10,11 +10,25 @@ LOCAL_PATH := device/xiaomi/gale
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Virtual A/B
+# Virtual AB
 ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Update Engine & Update Verifier 
+# Configure emulated_storage.mk
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 35
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
 PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
@@ -27,37 +41,30 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# A/B 
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
-
 PRODUCT_PACKAGES_DEBUG += \
-    bootctrl 
-
-PRODUCT_PACKAGES += \
-    bootctrl.mt6768 \
-    bootctrl.mt6768.recovery
-
-# Soong
-PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    update_engine_client
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.1-impl-mock \
     fastbootd
 
+# Bootctrl
+PRODUCT_PACKAGES_DEBUG += \
+    bootctrl \
+
 # Health HAL
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Additional Libraries
 TARGET_RECOVERY_DEVICE_MODULES += \
@@ -69,3 +76,12 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+
+# libion & libxml2
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion \
+    libxml2
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
